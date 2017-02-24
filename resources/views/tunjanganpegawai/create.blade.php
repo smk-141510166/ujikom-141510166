2bd1d15e7bab5258fcab35d859d30ca8dda10627
smk-141510166 <141510166@smkassalaambandung.sch.id>
2017-02-24 13:53:23 +0700

    perubahan dikit

--- resources/views/tunjanganpegawai/create.blade.php
+++ resources/views/tunjanganpegawai/create.blade.php
@@ -1,43 +1,42 @@
-@extends('layouts.bt')
-
 @section('content')
-	
+<h1><center>Tambah Tunjangan Pegawai</center></h1>
 <div class="container">
-	<div class="panel panel-primary">
-		<div class="panel-heading">Tambah Data Kategori Lembur</div>
-		<div class="panel-body">
-			<form method="POST" action="{{url('tunjanganpegawai')}}">
-			 	{{csrf_field()}}
-      
-                    <div class="control-group">
-                   <div class="control-group">
-                        <label class="control-label">Tunjangan</label>
-                        <div class="controls">
-                            <select class="form-control" name="kode_tunjangan_id">
-                                @foreach ($tunjangan as $data)
-                                <option value="{{ $data->id }}">{{ $data->tunjangan->kode_tunjangan_id }}</option>
-                                @endforeach
-                            </select>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-success">
+                <div class="panel-heading"></div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/tunjanganp') }}">
+                        {{ csrf_field() }}
+                      
+                        <div class="form-group{{ $errors->has('pegawai_id') ? ' has-error' : '' }}">
+                            <label for="pegawai_id" class="col-md-4 control-label">Pegawai </label>
+                            <div class="col-md-6">
+                                <select name="pegawai_id" class="form-control">
+                                    <option value="">pilih</option>
+                                    @foreach($pegawai as $data)
+                                    <option value="{{$data->id}}">{{$data->user->name}} &nbsp;&nbsp;&nbsp;&nbsp;{{$data->nip}}</option>
+                                    @endforeach
+                                </select>
+                                @if ($errors->has('pegawai_id'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('pegawai_id') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
                         </div>
-                    </div>
-
-                    <div class="control-group">
-                        <label class="control-label">Pegawai</label>
-                        <div class="controls">
-                            <select class="span11" name="id_user">
-                                @foreach ($pegawai as $data)
-                                <option value="{{ $data->id }}">{{ $data->user_id }}</option>
-                                @endforeach
-                            </select>
+                       
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-success form-control">
+                                    Simpan
+                                </button>
+                            </div>
                         </div>
-                    </div>
-
-				<div class="form-group">
-					<input class="btn btn-success" type="submit" name="submit" value="Tambah">
-				</div>
-			</form>
-		</div>
-	</div>
+                    </form>
+                </div>
+            </tunjangan>
+        </div>
+    </div>
 </div>
-
-@stop
+@endsection
--- resources/views/tunjanganpegawai/edit.blade.php
+++ resources/views/tunjanganpegawai/edit.blade.php
@@ -1,28 +1,44 @@
-@extends('layouts.bt')
-
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Update</div>
+<div class="col-md-9">
+        <div class="panel panel-info">
+        <div class="panel-heading"><h1><center><strong>Edit Data Tunjangan Pegawai</h1></strong></div>
+        <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('tunjanganpegawai.update',$tunjanganpegawai->id) }}">
+                        <input name="_method" type="hidden" value="PATCH">
+                        {{ csrf_field() }}
+                            
+                            <div class="form-group">
+                            <label for="id_pegawai" class="col-md-4 control-label">Nama Pegawai</label>
+                            <div class="col-md-6">
+                            <select class="form-control" name="id_pegawai" required>
+                            
+                            @foreach($pegawai as $data)
+                            <option value="{!! $data->id !!}">{!! $data->User->name !!}</option>
+                            @endforeach
+                            </select>
+                            </div>
+                            </div>
 
-                <div class="panel-body">
-                    {!! Form::model($tunjanganpegawai,['method' => 'PATCH','route'=>['tunjanganpegawai.update',$tunjanganpegawai->id]]) !!}
-                <div class="form-group">
-                    {!! Form::label('kode_tunjangan_id', 'Kode Tunjangan') !!}
-                    {!! Form::text('kode_tunjangan_id',$tunjanganpegawai->kode_tunjangan_id,['class'=>'form-control']) !!}
-                </div>
+                            <div class="form-group">
+                            <label for="kode_tunjangan" class="col-md-4 control-label">Uang Tunjangan</label>
+                            <div class="col-md-6">
+                            <select class="form-control" name="kode_tunjangan" required>
+                           
+                            @foreach($tunjangan as $data)
+                            <option value="{!! $data->id !!}">{!! $data->besaran_uang !!}</option>
+                            @endforeach
+                            </select>
+                            </div>
+                            </div>
 
-                <div class="form-group">
-                    {!! Form::label('id_pegawai', 'Id Pegawai') !!}
-                    {!! Form::text('id_pegawai',$tunjanganpegawai->kode_tunjangan_id,['class'=>'form-control']) !!}
-                </div>
-
-                <div class="form-group">
-                    {!! Form::submit('SAVE', ['class' => 'btn btn-primary']) !!}
-                </div>
-                {!! Form::close() !!}
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Simpan
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
